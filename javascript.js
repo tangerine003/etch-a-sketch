fb9570aa86ba5f1f155de0c36e849d9225cee33d
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-03-09 20:37:11 +0530

    Edit code to change size of grid according to user input

--- javascript.js
+++ javascript.js
@@ -1,10 +1,42 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const button = document.createElement("button");
+
+button.textContent = "Click Me";
+body.appendChild(button);
+body.appendChild(container);
+
+const squareDiv = {};
 
 for (i = 0; i < 16; i++) {
   for (j = 0; j < 16; j++) {
-    const squareDiv = {};
     squareDiv[j] = document.createElement("div");
     squareDiv[j].classList.add("square-div");
     container.appendChild(squareDiv[j]);
   }
 }
+
+button.addEventListener("click", () => {
+  const squareDivToRemove = document.querySelectorAll(".square-div");
+  squareDivToRemove.forEach((div) => {
+    container.removeChild(div);
+  });
+
+  let numOfSquareDivs = prompt(
+    "Enter number of squares per side of the new grid(Maximum number is 100)"
+  );
+
+  numOfSquareDivs = Number(numOfSquareDivs);
+
+  if (numOfSquareDivs <= 100) {
+    for (i = 0; i < numOfSquareDivs; i++) {
+      for (j = 0; j < numOfSquareDivs; j++) {
+        squareDiv[j] = document.createElement("div");
+        squareDiv[j].classList.add("square-div");
+        squareDiv[j].style.width = `${1232 / numOfSquareDivs}px`;
+        squareDiv[j].style.height = `${1232 / numOfSquareDivs}px`;
+        container.appendChild(squareDiv[j]);
+      }
+    }
+  }
+});
