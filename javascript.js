b33160da75832c8bcbdb1b3b65abd60ca8701245
tangerine003 <123626883+tangerine003@users.noreply.github.com>
2023-03-13 19:05:27 +0530

    Put code to draw and paint grid into respective functions and call them

--- javascript.js
+++ javascript.js
@@ -17,37 +17,7 @@ for (let i = 0; i < 16; i++) {
   }
 }
 
-/* eachSquareDiv.forEach((div) => {
-  div.addEventListener("mousedown", () => {
-    div.classList.add("change-color");
-  });
-});
- */
-
-/* button.addEventListener("click", () => {
-  const squareDivToRemove = document.querySelectorAll(".square-div");
-  squareDivToRemove.forEach((div) => {
-    container.removeChild(div);
-  });
-
-  let numOfSquareDivs = prompt(
-    "Enter number of squares per side of the new grid(Maximum number is 100)"
-  );
-
-  numOfSquareDivs = Number(numOfSquareDivs);
-
-  if (numOfSquareDivs <= 100) {
-    for (let i = 0; i < numOfSquareDivs; i++) {
-      for (let j = 0; j < numOfSquareDivs; j++) {
-        squareDiv[j] = document.createElement("div");
-        squareDiv[j].classList.add("square-div");
-        squareDiv[j].style.width = `${1232 / numOfSquareDivs}px`;
-        squareDiv[j].style.height = `${1232 / numOfSquareDivs}px`;
-        container.appendChild(squareDiv[j]);
-      }
-    }
-  }
-}); */
+paintGrid();
 
 /* GRID SLIDER */
 let gridSlider = document.getElementById("myRange");
@@ -57,13 +27,15 @@ output.textContent = gridSlider.value;
 gridSlider.oninput = function () {
   console.log(this.value);
   output.textContent = this.value;
+  drawGrid(this.value);
+  paintGrid();
+};
 
+function drawGrid(numOfSquareDivs) {
   const squareDivToRemove = document.querySelectorAll(".square-div");
   squareDivToRemove.forEach((div) => {
     container.removeChild(div);
   });
-
-  let numOfSquareDivs = this.value;
 
   if (numOfSquareDivs <= 100) {
     for (let i = 0; i < numOfSquareDivs; i++) {
@@ -76,20 +48,20 @@ gridSlider.oninput = function () {
       }
     }
   }
-};
+}
 
-const eachSquareDiv = document.querySelectorAll(".square-div");
-eachSquareDiv.forEach((div) => {
-  div.addEventListener("mousemove", draw);
-  console.log("HI");
-  div.addEventListener("mousedown", draw);
-  console.log("HI again");
-});
+function paintGrid() {
+  const eachSquareDiv = document.querySelectorAll(".square-div");
+  eachSquareDiv.forEach((div) => {
+    div.addEventListener("mousemove", draw);
+    div.addEventListener("mousedown", draw);
+  });
 
-function draw(e) {
-  if (e.button == 0) {
-    e.target.style.backgroundColor = "black";
-  } else {
-    return;
+  function draw(e) {
+    if (e.button == 0) {
+      e.target.style.backgroundColor = "black";
+    } else {
+      return;
+    }
   }
 }
